46c7e010eebcd43b9d0e5311d8578acf5382a5a8
mahmudurbd <srahmandiu@gmail.com>
2024-09-03 23:22:43 +0600

    Add jwt token api and fixed cors orign issues

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ import express from "express";
 import cors from "cors";
 import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+// app.use(cors({ origin: ["http://localhost:5173/"], credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"], // The exact origin of your frontend
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  })
+);
+
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = process.env.MONGO_URI;
 
@@ -22,64 +32,85 @@ const client = new MongoClient(uri, {
   },
 });
 
-const database = client.db("AutoCareDB");
-const serviceCollection = database.collection("services");
-const bookingsCollection = database.collection("bookings");
-
-// Services GET API
-app.get("/services", async (req, res) => {
-  console.log("services get api is hitting");
-  const services = serviceCollection.find();
-  const result = await services.toArray();
-  res.send(result);
-});
-
-// Service Details GET API
-app.get("/services/:id", async (req, res) => {
-  console.log("Service Details api");
-  const id = req.params.id;
-  const query = { _id: new ObjectId(id) };
-  const result = await serviceCollection.findOne(query);
-  res.send(result);
-});
-
-// Booking GET API
-app.get("/bookings", async (req, res) => {
-  console.log("Booking get api hitting");
-  console.log(req.query);
-  let query = {};
-  if (req.query?.email) {
-    query = {
-      email: req.query.email,
-    };
-  }
-  const result = await bookingsCollection.find(query).toArray();
-  res.send(result);
-});
-
-// Booking POST API
-app.post("/bookings", async (req, res) => {
-  console.log("Bookings api hitting");
-  const booking = req.body;
-  console.log(booking);
-  const result = await bookingsCollection.insertOne(booking);
-  res.send(result);
-});
-
-// Booking DELETE API
-app.delete("/bookings/:id", async (req, res) => {
-  console.log("Booking Delete api hitting");
-  const id = req.params.id;
-  console.log(id);
-  const query = { _id: new ObjectId(id) };
-  const result = await bookingsCollection.deleteOne(query);
-  res.send(result);
-});
-
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("AutoCareDB");
+    const serviceCollection = database.collection("services");
+    const bookingsCollection = database.collection("bookings");
+
+    // Auth Service API
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log("token api is hitting", user);
+      const token = jwt.sign({ data: user }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+          // sameSite: "None",
+          // maxAge: 24 * 60 * 60 * 1000,
+          // path: "/",
+        })
+        .send({ success: true });
+    });
+
+    // Services GET API
+    app.get("/services", async (req, res) => {
+      console.log("services get api is hitting");
+      const services = serviceCollection.find();
+      const result = await services.toArray();
+      res.send(result);
+    });
+
+    // Service Details GET API
+    app.get("/services/:id", async (req, res) => {
+      console.log("Service Details api");
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Booking GET API
+    app.get("/bookings", async (req, res) => {
+      console.log("Booking get api hitting");
+      console.log("Request query:", req.query);
+      console.log("Cookies:", req.cookies); // Log all cookies
+      console.log("Token:", req.cookies.token);
+      let query = {};
+      if (req.query?.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const result = await bookingsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // Booking POST API
+    app.post("/bookings", async (req, res) => {
+      console.log("Bookings api hitting");
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingsCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    // Booking DELETE API
+    app.delete("/bookings/:id", async (req, res) => {
+      console.log("Booking Delete api hitting");
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
